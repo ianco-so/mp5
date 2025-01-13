7c944a37be7a1202b7bad321eb40085b41444675
ianco <ianco.so@gmail.com>
2025-01-13 12:30:46 -0300

    :bug: fix(laucher): Problema do launcher resolvido

--- lib/screens/place_detail_screen.dart
+++ lib/screens/place_detail_screen.dart
@@ -7,28 +7,40 @@ class PlaceDetailScreen extends StatelessWidget {
 
   PlaceDetailScreen(this.place);
 
+  // Future<void> _launchPhone(String phone) async {
+  //   final url = 'tel:$phone';
+  //   if (await canLaunch(url)) {
+  //     await launch(url);
+  //   } else {
+  //     throw 'Could not launch $url';
+  //   }
+  // }
+
   Future<void> _launchPhone(String phone) async {
-    final url = 'tel:$phone';
-    if (await canLaunch(url)) {
-      await launch(url);
+    // final url = 'tel:$phone';
+    final url = Uri.parse('tel:+55$phone');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
     } else {
       throw 'Could not launch $url';
     }
   }
 
   Future<void> _launchEmail(String email) async {
-    final url = 'mailto:$email';
-    if (await canLaunch(url)) {
-      await launch(url);
+    // final url = 'mailto:$email';
+    final url = Uri.parse('mailto:$email');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
     } else {
       throw 'Could not launch $url';
     }
   }
 
   Future<void> _launchMap(double lat, double lng) async {
-    final url = 'https://www.google.com/maps/search/?api=1&query=$lat,$lng';
-    if (await canLaunch(url)) {
-      await launch(url);
+    // final url = 'https://www.google.com/maps/search/?api=1&query=$lat,$lng';
+    final url = Uri.parse('https://www.google.com/maps/search/?api=1&query=$lat,$lng');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
     } else {
       throw 'Could not launch $url';
     }
